c2142ef43cc339818252771c4f2a1cd225e1d7b5
Yves P <havokinspiration@gmail.com>
2018-03-13 10:57:43 +0100

    Create a plugin class + update the composer.json + removes the "config" files (configuration is now done in the plugin class)

--- config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-use SubsGuru\Core\Payments\PaymentGatewayRepository;
-
-//
-// Registering SEPA payment handler
-//
-PaymentGatewayRepository::add('SubsGuru\\SEPA\\Payments\\Gateway\\SEPAPaymentGateway');
---
+++ src/SepaPaymentPlugin.php
@@ -0,0 +1,45 @@
+<?php
+namespace SubsGuru\SEPA;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+use Cake\Routing\Route\DashedRoute;
+use Cake\Routing\RouteBuilder;
+use Cake\Routing\Router;
+use SubsGuru\Core\Payments\PaymentGatewayRepository;
+
+class SepaPaymentPlugin extends BasePlugin
+{
+    public function middleware($middleware)
+    {
+        // Add middleware here.
+        return $middleware;
+    }
+
+    public function console($commands)
+    {
+        // Add console commands here.
+        return $commands;
+    }
+
+    public function bootstrap(PluginApplicationInterface $app)
+    {
+        parent::bootstrap($app);
+
+        PaymentGatewayRepository::add('SubsGuru\\SEPA\\Payments\\Gateway\\SEPAPaymentGateway');
+    }
+
+    public function routes($routes)
+    {
+        parent::routes($routes);
+
+        Router::plugin(
+            'SubsGuru/SEPA',
+            ['path' => '/sepa'],
+            function (RouteBuilder $routes) {
+                $routes->fallbacks(DashedRoute::class);
+            }
+        );
+
+    }
+}
